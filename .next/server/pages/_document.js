8e919aa91ff8ded581f2e23ef96f47b0f46fe8dc
heegupark <slhead@gmail.com>
2020-09-13 22:38:10 -0700

    removed unnessary files

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -100,7 +100,7 @@ module.exports = require("styled-jsx/server");
 
 /***/ }),
 
-/***/ 4:
+/***/ 3:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__("VDXt");
