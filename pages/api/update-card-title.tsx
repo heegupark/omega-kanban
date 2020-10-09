29ffdd07dc9df27831c56d127cdf7f2620adc499
heegupark <slhead@gmail.com>
2020-10-08 22:57:41 -0700

    update card title endpoint is done

--- pages/api/update-card-title.tsx
+++ pages/api/update-card-title.tsx
@@ -1,15 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import Column from '../../middleware/models/column';
+import Card from '../../middleware/models/card';
 require('../../middleware/db/mongoose');
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   const { _id, title } = request.body;
   try {
-    await Column.updateOne({ _id }, { $set: { title } });
+    await Card.updateOne({ _id }, { $set: { title } });
     return response.status(200).json({ success: true });
   } catch (e) {
     return response
       .status(500)
-      .json({ success: false, message: 'failed to update a column title' });
+      .json({ success: false, message: 'failed to update a card title' });
   }
 };
